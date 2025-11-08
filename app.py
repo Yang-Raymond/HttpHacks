77894d5082fcdc7f87f03b45d7688e90e77ca9d3
Raymond <raymondyang59@gmail.com>
2025-11-08 15:08:23 -0800

    created base app

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('App name')
 
 app = QApplication([])
-
-window = QWidget()
-window.setWindowTitle('App name')
-
-#
-
+window = MainWindow()
 window.show()
 app.exec()
